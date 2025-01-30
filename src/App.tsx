a744e32e359c27b243690ed3e8a16c3bbbc7be80
saurabhraut1212 <saurabhrmahadev1212@gmail.com>
2025-01-30 12:29:38 +0530

    feat: initialise app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div >
+     <h1 className="text-3xl font-bold color-red-500">
+    Hello world!
+  </h1> 
+    </div>
+  )
+}
+
+export default App
